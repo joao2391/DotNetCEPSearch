bf38d066af2d1f1a9f0cd2b16cf9eccca31c9da9
joaopaulo2391 <j_paulo_15@hotmail.com>
2020-04-22 07:53:41 -0300

    Added GetCepByAddress function

--- src/DotNet.CEP.Search/DotNet.CEP.Search.App/CepSearch.cs
+++ src/DotNet.CEP.Search/DotNet.CEP.Search.App/CepSearch.cs
@@ -132,5 +132,232 @@ namespace DotNet.CEP.Search.App
             }
         }
 
+        public async Task<string> GetCepByAddressAsync(string address)
+        {
+            try
+            {
+                var lstEnderecos = new List<ResponseEndereco>();
+                bool hasNextPage = false;
+                var hsRespEndereco = new HashSet<ResponseEndereco>();
+                var dict = new Dictionary<string, string>
+                {
+                    {"relaxation",address},
+                    {"tipoCEP","ALL" },
+                    {"semelhante","N" }
+                };
+
+                var httpRequest = new HttpRequestMessage(HttpMethod.Post, UrlCorreio)
+                {
+                    Content = new FormUrlEncodedContent(dict)
+                };
+
+                var httpResponse = await _client.SendAsync(httpRequest).Result.Content.ReadAsStringAsync();
+                var html = httpResponse;
+
+                var htmlDoc = new HtmlDocument();
+                htmlDoc.LoadHtml(html);
+                var link = htmlDoc.DocumentNode.SelectNodes("//div//a");
+
+                if (link == null)
+                {
+                    return Constants.NOT_FOUND_MESSAGE;
+                }
+
+                for (int i = 0; i < link.Count; i++)
+                {
+                    if (link[i].InnerHtml.Contains("[ Próxima ]"))
+                    {
+                        hasNextPage = true;
+                    }
+                }
+
+                var name = htmlDoc.DocumentNode.SelectNodes("//td");
+                var divCtrlContent = htmlDoc.DocumentNode.SelectNodes("//div[@class='ctrlcontent']");
+                var numPages = divCtrlContent[0].OuterHtml.Substring(1068, 13);
+
+                if (name == null)
+                {
+                    return Constants.NOT_FOUND_MESSAGE;
+                }
+
+                for (int i = 0; i < name.Count; i++)
+                {
+                    if (i % 4 == 0)
+                    {
+                        var reponseEndereco = new ResponseEndereco
+                        {
+                            Bairro = name[i + 1].InnerText.Replace("&nbsp;", string.Empty),
+                            Cep = name[i + 3].InnerText.Replace("&nbsp;", string.Empty),
+                            Cidade = name[i + 2].InnerText.Replace("&nbsp;", string.Empty),
+                            Rua = name[i].InnerText.Replace("&nbsp;", string.Empty),
+                        };
+
+                        hsRespEndereco.Add(reponseEndereco);
+                    }
+                }
+
+                if (hasNextPage)
+                {
+                    AcessaProximasPaginas(hsRespEndereco, address, numPages);
+                }
+
+                return JsonConvert.SerializeObject(hsRespEndereco);
+
+            }
+            catch (Exception ex)
+            {
+                return string.Concat(Constants.ERROR_MESSAGE, ex.Message);
+            }
+        }
+
+        public string GetCepByAddress(string address)
+        {
+            try
+            {
+                var lstEnderecos = new List<ResponseEndereco>();
+                bool hasNextPage = false;
+                var hsRespEndereco = new HashSet<ResponseEndereco>();
+                var dict = new Dictionary<string, string>
+                {
+                    {"relaxation",address},
+                    {"tipoCEP","ALL" },
+                    {"semelhante","N" }
+                };
+
+                var httpRequest = new HttpRequestMessage(HttpMethod.Post, UrlCorreio)
+                {
+                    Content = new FormUrlEncodedContent(dict)
+                };
+
+                var httpResponse = _client.SendAsync(httpRequest).Result.Content.ReadAsStringAsync().Result;
+                var html = httpResponse;
+
+                var htmlDoc = new HtmlDocument();
+                htmlDoc.LoadHtml(html);
+                var link = htmlDoc.DocumentNode.SelectNodes("//div//a");
+
+                if (link == null)
+                {
+                    return Constants.NOT_FOUND_MESSAGE;
+                }
+
+                for (int i = 0; i < link.Count; i++)
+                {
+                    if (link[i].InnerHtml.Contains("[ Próxima ]"))
+                    {
+                        hasNextPage = true;
+                    }
+                }
+
+                var name = htmlDoc.DocumentNode.SelectNodes("//td");
+                var divCtrlContent = htmlDoc.DocumentNode.SelectNodes("//div[@class='ctrlcontent']");
+                var numPages = divCtrlContent[0].OuterHtml.Substring(1068, 13);
+
+                if (name == null)
+                {
+                    return Constants.NOT_FOUND_MESSAGE;
+                }
+
+                for (int i = 0; i < name.Count; i++)
+                {
+                    if (i % 4 == 0)
+                    {
+                        var reponseEndereco = new ResponseEndereco
+                        {
+                            Bairro = name[i + 1].InnerText.Replace("&nbsp;", string.Empty),
+                            Cep = name[i + 3].InnerText.Replace("&nbsp;", string.Empty),
+                            Cidade = name[i + 2].InnerText.Replace("&nbsp;", string.Empty),
+                            Rua = name[i].InnerText.Replace("&nbsp;", string.Empty),
+                        };
+
+                        hsRespEndereco.Add(reponseEndereco);
+                    }
+                }
+
+                if (hasNextPage)
+                {
+                    AcessaProximasPaginas(hsRespEndereco, address, numPages);
+                }
+
+                return JsonConvert.SerializeObject(hsRespEndereco);
+
+            }
+            catch (Exception ex)
+            {
+                return string.Concat(Constants.ERROR_MESSAGE, ex.Message);
+            }
+        }
+
+
+        #region Private Methods
+
+        private void AcessaProximasPaginas(HashSet<ResponseEndereco> hsRespEnd, string endereco,
+                                            string numPages, int pageIni = 51, int pageFim = 100)
+        {
+            bool hasNextPage = false;
+            var dict = new Dictionary<string, string>
+            {
+                {"relaxation", endereco},
+                {"tipoCEP", "ALL"},
+                {"semelhante", "N"},
+                {"qtdrow", "50"},
+                {"pagIni", pageIni.ToString()},
+                {"pagFim", pageFim.ToString()}
+            };
+
+            var httpRequest = new HttpRequestMessage(HttpMethod.Post, UrlCorreio)
+            {
+                Content = new FormUrlEncodedContent(dict)
+            };
+
+            var httpResponse = _client.SendAsync(httpRequest).Result.Content.ReadAsStringAsync();
+
+            var html = httpResponse.Result;
+
+            var docHtml = new HtmlDocument();
+            docHtml.LoadHtml(html);
+
+            var name = docHtml.DocumentNode.SelectNodes("//td");
+
+            if (!hasNextPage)
+            {
+                var link = docHtml.DocumentNode.SelectNodes("//div//a");
+
+                for (int i = 0; i < link.Count; i++)
+                {
+                    if (link[i].InnerHtml.Contains("[ Próxima ]"))
+                    {
+                        hasNextPage = true;
+                        pageIni += 25;
+                        pageFim += 25;
+                    }
+                }
+            }
+
+            for (int i = 0; i < name.Count; i++)
+            {
+                if (i % 4 == 0)
+                {
+                    var reponseEndereco = new ResponseEndereco
+                    {
+                        Bairro = name[i + 1].InnerText.Replace("&nbsp;", string.Empty),
+                        Cep = name[i + 3].InnerText.Replace("&nbsp;", string.Empty),
+                        Cidade = name[i + 2].InnerText.Replace("&nbsp;", string.Empty),
+                        Rua = name[i].InnerText.Replace("&nbsp;", string.Empty),
+                    };
+
+                    hsRespEnd.Add(reponseEndereco);
+                }
+            }
+
+            if (hasNextPage)
+            {
+                AcessaProximasPaginas(hsRespEnd, endereco, numPages, pageIni, pageFim);
+            }
+
+
+        }
+
+        #endregion
     }
 }
--- tests/DotNet.CEP.Search.Test/CepSearchTests.cs
+++ tests/DotNet.CEP.Search.Test/CepSearchTests.cs
@@ -9,6 +9,7 @@ namespace Tests
 
         CepSearch cep;
         string validCep = "12515520";
+        string validAddress = "Rua Fernandes Vieira";
 
         [SetUp]
         public void Setup()
@@ -19,7 +20,7 @@ namespace Tests
         [Test]
         public async Task Should_Return_A_Valid_Address_Async()
         {
-            var result = await cep.GetAddressByCepAsync(validCep);
+            var result = await cep.GetAddressByCepAsync(validCep);            
 
             Assert.IsNotNull(result);
         }
@@ -28,6 +29,22 @@ namespace Tests
         public void Should_Return_A_Valid_Address()
         {
             var result = cep.GetAddressByCep(validCep);
+
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public async Task Should_Return_A_Valid_Cep_Async()
+        {
+            var result = await cep.GetCepByAddressAsync(validAddress);
+
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void Should_Return_A_Valid_Cep()
+        {
+            var result = cep.GetCepByAddress(validAddress);
 
             Assert.IsNotNull(result);
         }
